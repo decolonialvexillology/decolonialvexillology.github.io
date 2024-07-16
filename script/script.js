a3fad3cee1a8900b55cc68faefd76c1ae9d4fb20
Miro <miropaxjones@gmail.com>
2024-07-16 14:03:37 +1000

    Create script.js

---
+++ script/script.js
@@ -0,0 +1,7 @@
+function openIframeModal() {
+  document.getElementById('iframeModal').style.width = 100%;
+}
+
+function closeIframeModal() {
+  document.getElementById('iframeModal').style.width = 100%;
+}
