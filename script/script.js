0b364cdde71223e4afeb5aea14ab1f9a5d8f3638
Miro <miropaxjones@gmail.com>
2024-07-16 14:31:45 +1000

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,17 @@
+// Navigation
 function openNav() {
   document.getElementById("mySidenav").style.width = "100%";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+}
+
+// Settings
+function openSet() {
+  document.getElementById("settings").style.width = "100%";
+}
+
+function closeSet() {
+  document.getElementById("settings").style.width = "0";
 }
